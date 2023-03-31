1db0f5c80848750813fcf26ff2f5fb8d053ab89e
Anmol Verma <115978470+Anmol26verma@users.noreply.github.com>
2023-03-31 23:08:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,54 +1,38 @@
-let weather = {
-  apiKey: "API KEY GOES HERE",
-  fetchWeather: function (city) {
-    fetch(
-      "https://api.openweathermap.org/data/2.5/weather?q=" +
-        city +
-        "&units=metric&appid=" +
-        this.apiKey
-    )
-      .then((response) => {
-        if (!response.ok) {
-          alert("No weather found.");
-          throw new Error("No weather found.");
-        }
-        return response.json();
-      })
-      .then((data) => this.displayWeather(data));
-  },
-  displayWeather: function (data) {
-    const { name } = data;
-    const { icon, description } = data.weather[0];
-    const { temp, humidity } = data.main;
-    const { speed } = data.wind;
-    document.querySelector(".city").innerText = "Weather in " + name;
-    document.querySelector(".icon").src =
-      "https://openweathermap.org/img/wn/" + icon + ".png";
-    document.querySelector(".description").innerText = description;
-    document.querySelector(".temp").innerText = temp + "°C";
-    document.querySelector(".humidity").innerText =
-      "Humidity: " + humidity + "%";
-    document.querySelector(".wind").innerText =
-      "Wind speed: " + speed + " km/h";
-    document.querySelector(".weather").classList.remove("loading");
-    document.body.style.backgroundImage =
-      "url('https://source.unsplash.com/1600x900/?" + name + "')";
-  },
-  search: function () {
-    this.fetchWeather(document.querySelector(".search-bar").value);
-  },
-};
+// Set up API key
+const apiKey = "9f793029da21457d8c5183408232703";
 
-document.querySelector(".search button").addEventListener("click", function () {
-  weather.search();
-});
+// Function to get weather data from API
+async function getWeather(city) {
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=$9f793029da21457d8c5183408232703&q=$Delhi&aqi=no`);
+  const data = await response.json();
+  return data;
+}
 
-document
-  .querySelector(".search-bar")
-  .addEventListener("keyup", function (event) {
-    if (event.key == "Enter") {
-      weather.search();
-    }
-  });
+// Function to update UI with weather data
+function updateUI(data) {
+  const city = data.location.name;
+  const temp = data.current.temp_c;
+  const desc = data.current.condition.text;
 
-weather.fetchWeather("Denver");
+  const cityElem = document.getElementById("city");
+  const tempElem = document.getElementById("temp");
+  const descElem = document.getElementById("desc");
+
+  cityElem.textContent = city;
+  tempElem.textContent = `${temp}°C`;
+  descElem.textContent = desc;
+}
+
+// Function to handle form submission
+async function handleSubmit(event) {
+  event.preventDefault();
+  const city = document.getElementById("cityInput").value;
+  const data = await getWeather(city);
+  updateUI(data);
+}
+
+// Add event listener to form
+const form = document.getElementById("weatherForm");
+form.addEventListener("submit", handleSubmit);
+
+
